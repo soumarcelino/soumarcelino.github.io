b1c2b5519db0a59386438b409c5a41a77a86ed0c
soumarcelino <marcelinomatiasdesouza@gmail.com>
2019-09-09 23:06:49 -0300

    Updates

---
+++ precache-manifest.f44b956b56a8358476e5cef46d7048ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b34d91dc6b590a59d7d86efa57f3953",
+    "url": "/index.html"
+  },
+  {
+    "revision": "181ca2c4ac96f197270c",
+    "url": "/static/css/2.8c467cd4.chunk.css"
+  },
+  {
+    "revision": "378ec9c41dcc96c70bf3",
+    "url": "/static/css/main.5c769955.chunk.css"
+  },
+  {
+    "revision": "181ca2c4ac96f197270c",
+    "url": "/static/js/2.6f7f6ffc.chunk.js"
+  },
+  {
+    "revision": "378ec9c41dcc96c70bf3",
+    "url": "/static/js/main.791a9c79.chunk.js"
+  },
+  {
+    "revision": "ed3c0d4f887e652dd63b",
+    "url": "/static/js/runtime~main.dc724140.js"
+  }
+]);
